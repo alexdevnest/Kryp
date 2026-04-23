6a7288cb82171ebfaa8b012c416ecce67e06eb6a
Alex Kimari <officialalex236@gmail.com>
2026-04-23 16:25:22 +0300

    Implemented candlestick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,29 +1,41 @@
 import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
+import CandlestickChart from '@/components/CandlestickChart';
 
 
 export default async function CoinOverview () {
-  const coin = await fetcher<CoinDetailsData> ('/coins/bitcoin', {
-      dex_pair_format: 'symbol'
-  })
+  const [ coin, coinOHLCData ] = await Promise.all([
+    fetcher<CoinDetailsData> ('/coins/bitcoin', {
+      dex_pair_format: 'symbol',
+    }),
+
+    fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
+      vs_currency: 'usd',
+      days: 1,
+      precision: 'full',
+    }),
+  ]);
+
 
   return (
     <div id="coin-overview">
-          <div className="header pt-2">
-            <Image
-              src={coin.image.large}
-              alt={coin.name}
-              width={50}
-              height={50}
-            />
-            <div className="info">
-              <p>{ coin.name } / { coin.symbol.toUpperCase() }</p>
-              <h1>
-                { formatCurrency(coin.market_data.current_price.usd) }
-              </h1>
-            </div>
+      <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={50}
+            height={50}
+          />
+          <div className="info">
+            <p>{ coin.name } / { coin.symbol.toUpperCase() }</p>
+            <h1>
+              { formatCurrency(coin.market_data.current_price.usd) }
+            </h1>
           </div>
         </div>
-  );
+      </CandlestickChart>
+    </div>
+);
 }
